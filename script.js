26517bd25cf5a1f5d6781c595f3d11cff73f9c66
Nikita Jagdale <128221436+NikitaJagdale88@users.noreply.github.com>
2023-05-17 16:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,23 @@
 
 function palindrome(str){
 
+	// let arr=str.split(" ");
+ //    let s="";
+	//  arr.forEach((str)=>{
+	// 	  if(str!=''){
+ //           s=s+str;
+	// 	  }
+	//  })
+	let s=str.toLowerCase();
+let start=0;
+let end=s.length-1;
+	while(start<end){
+		if(s[start]!=s[end]){
+			return false;
+		}
+		start++;
+		end--;
+	}
+	return true;
 }
 module.exports = palindrome
